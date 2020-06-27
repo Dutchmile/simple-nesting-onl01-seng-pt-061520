6147f77f11f49df58e84b39bbe9b9bd505c0d95f
Christopher Dutcher <cadutcher@icloud.com>
2020-06-27 11:51:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages].split
+		 dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
