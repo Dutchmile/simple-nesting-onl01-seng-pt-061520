fbbf1e5386be8a929bc1f9b3af56663c4f9392f5
Christopher Dutcher <cadutcher@icloud.com>
2020-06-27 11:56:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmers_hash[:yukihiro_matsumoto] = {}
-programmers_hash
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash
 end
 
 def changing_alan
